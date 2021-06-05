b9bddc31e4a5d3578bef045ee1ebbd12955b4f11
Manoela Cassia <manoelacs@ic.ufal.br>
2021-06-05 11:27:26 -0300

    first-commit

---
+++ collier-digital/src/App.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+//import Routes from './routes/Routes';
+import 'antd/dist/antd.less';
+
+const App: FC = () => (
+  <div className="App">
+     {/*  <Routes/> */}
+     App
+  </div>
+);
+
+export default App;
